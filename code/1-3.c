3ce07896284bd6335d89d1bf1b889b24da3522d0
ljw2044 <144752394+ljw2044@users.noreply.github.com>
2026-04-03 09:25:13 +0900

    1-3c

--- code/1-3.c
+++ code/1-3.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 
-void print_reverse(int *arr, int *size) {
-    for(int i = *size - 1; i >= 0; i--) {
-        printf("%d ", arr[i]);
+void print_reverse(int *arr, int size) {
+    int *ptr = arr + size - 1;
+
+    for (int i = 0; i < size; i++) {
+        printf("%d", *ptr);
+        
+        if (ptr > arr) {
+            printf(" ");
+        }
+        
+        ptr--;
     }
 }
 
 int main() {
-    int nums[]  = {1, 2, 3, 4, 5};
+    int nums[] = {1, 2, 3, 4, 5};
 
     print_reverse(nums, 5);
     printf("\n");
